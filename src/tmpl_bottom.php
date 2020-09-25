5e400abf11336f47bc48169e9cee665e9f4d8e9c
Calibet Yann <yanncalibet@gmail.com>
2020-09-25 20:38:11 +0200

    migration sous git

---
+++ src/tmpl_bottom.php
@@ -0,0 +1,26 @@
+<hr>
+</main>
+  <footer>
+    <nav class="container_logo_nav">
+      <div>
+        <ul class="nav_bar">
+          <li class="liens_menu"><a href="index.phtml">accueil</a></li>
+          <li class="liens_menu"><a href="contact.phtml">contact</a></li>
+          <li class="liens_menu"><a href="equipe.phtml">notre équipe</a></li>
+          <li class="liens_menu"><a href="livre_or.phtml">livre d'or</a></li>
+        </ul>  
+      </div>
+      <ul class="nav_bar">
+        <li><i class="fab fa-facebook-square fa-2x"></i></li>
+        <li><i class="fab fa-instagram fa-2x"></i></li>
+        <li><i class="fab fa-twitter fa-2x"></i></li>
+      </ul>
+    </nav>
+    <p class="footer">
+    <b>LY voyages</b> Thaïlande,96 rue Servan 75544 Paris Cedex 11<br>
+    infos@lyvoyages.com - ‎06 90 56 78 23 - <span>www.lyvoyages.com</span><br>
+    Siret : 844 563 878 TVA : FR 35 844 563 878 
+    </p>
+  </footer>
+</body>
+</html>
---
+++ src/tmpl_top.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/normalize.css">
+    <title>LY VOYAGES</title>
+    <link rel="stylesheet" href="css/style_voyages.css">
+    <link rel="stylesheet" href="css/style2.css">
+    <script src="https://kit.fontawesome.com/f2574f15cd.js" crossorigin="anonymous"></script>
+</head>
+<body>
+  <header>
+    <nav class="container_logo_nav">
+      <div>
+          <a href="index.phtml"><img class="ly_logo" src="img/LY_logo.jpg" alt="ly_logo"></a>
+      </div>
+      <ul class="nav_bar">
+        <li class="liens_menu"><a href="index.phtml">accueil</a></li>
+        <li class="liens_menu"><a href="contact.phtml">contact</a></li>
+        <li class="liens_menu"><a href="equipe.phtml">notre équipe</a></li>
+        <li class="liens_menu"><a href="livre_or.phtml">livre d'or</a></li>
+      </ul>
+    </nav>
+      <h1 class="titre_du_site"><a href="index.phtml">LY VOYAGES</a></h1>
+    </div>
+  </header>
+  <main>
+  <hr>
